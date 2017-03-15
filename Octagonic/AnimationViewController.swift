8377d1a93d566e7babc53170570c47a192e99cff
Random Guy <deel@UsDiTo.local>
2017-03-15 23:20:37 +0100

    Image picker implemented. TimeInterval sending implemented. And some minor changes to UI (texts, constrains and etc.)

--- Octagonic/AnimationViewController.swift
+++ Octagonic/AnimationViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  AnimationViewController.swift
 //  Octagonic
 //
 //  Created by Deel Usmani on 15/03/2017.
@@ -8,31 +8,31 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class AnimationViewController: UIViewController {
     
     var octagon: Octagonic?
+    var pickedImage: UIImage?
+    var animTime: TimeInterval?
     //var convertedRectView: CGRect!
-   
+    
     @IBOutlet weak var animationView: UIView!
-
-
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         //convertedRectView = viewUnder.convert(animationView.frame, to: self.view)
-
-        let img = UIImageView(image: UIImage(named: "buxoro.jpg"))
+        
+        let img = UIImageView(image: pickedImage ?? UIImage(named: "buxoro.jpg"))
         
         self.view.backgroundColor = UIColor.white
         octagon = Octagonic(view: self.animationView, image: img, color: UIColor.white, offset: 0)
-        octagon?.animateOctagon(duration: 3)
+        octagon?.animateOctagon(duration: animTime ?? 1)
     }
-    
-    @IBAction func backAction(_ sender: Any) {
+    @IBAction func back(_ sender: Any) {
         dismiss(animated: true, completion: nil)
     }
-
-  
-
+    
+    
 
 }
--- Octagonic/StartViewController.swift
+++ Octagonic/StartViewController.swift
@@ -8,23 +8,48 @@
 
 import UIKit
 
-class StartViewController: UIViewController {
+class StartViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     @IBOutlet weak var timeSlider: UISlider!
     @IBOutlet weak var animationTimeLabel: UILabel!
     @IBOutlet weak var chooseImageButton: UIButton!
     @IBOutlet weak var octagonitButton: UIButton!
+    @IBOutlet weak var chosenImageStatus: UILabel!
+    
+    let imagePicker = UIImagePickerController()
+    
+    var pickedImage: UIImage?
+    var animationTime: Int?
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
         octagonitButton.layer.cornerRadius = 7
         chooseImageButton.layer.cornerRadius = 7
+        imagePicker.delegate = self
     }
 
     @IBAction func chooseImageAction(_ sender: Any) {
+        imagePicker.allowsEditing = false
+        imagePicker.sourceType = .photoLibrary
+        
+        present(imagePicker, animated: true, completion: nil)
+    }
     
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        if let chosenImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            //imageView.contentMode = .ScaleAspectFit
+            pickedImage = chosenImage
+            self.chosenImageStatus.text = "Loaded"
+            //print(info)
+        }
+        
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+         dismiss(animated: true, completion: nil)
     }
   
     @IBAction func octagonItAction(_ sender: Any) {
@@ -35,11 +60,16 @@ class StartViewController: UIViewController {
         let output = timeSlider.value
         let newValue = 5 * floor((output/5)+0.5)
         timeSlider.value = newValue
-        animationTimeLabel.text = "Animation lasts: \(timeSlider.value)"
+        animationTimeLabel.text = "Animation lasts: \(timeSlider.value) sec"
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "octagonifySegue" {
+            //We have navcontroller on our way to AnimationVC
+            let destinationNavigationController = segue.destination as! UINavigationController
+            let targetController = destinationNavigationController.topViewController as! AnimationViewController
+            targetController.pickedImage = self.pickedImage
+            targetController.animTime = Double(self.timeSlider.value)
             
         }
     }
