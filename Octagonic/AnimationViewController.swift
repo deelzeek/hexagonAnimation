de6c06fe533efbf8b856eeba26b136ebae80072a
Random Guy <deel@UsDiTo.local>
2017-03-16 10:16:14 +0100

    Landscape mode adapted for starter view. Landscape mode implemented for AnimationCV, Octagonic class should be updated.

--- Octagonic/AnimationViewController.swift
+++ Octagonic/AnimationViewController.swift
@@ -15,13 +15,21 @@ class AnimationViewController: UIViewController {
     var octagon: Octagonic?
     var pickedImage: UIImage?
     var animTime: TimeInterval?
+    var vW: CGFloat?
+    var vH: CGFloat?
     //var convertedRectView: CGRect!
     
     @IBOutlet weak var animationView: UIView!
+    @IBOutlet var animViewHeight: NSLayoutConstraint!
     
+    @IBOutlet var animViewWidth: NSLayoutConstraint!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let screenSize = UIScreen.main.bounds
+        vW = screenSize.width
+        vH = screenSize.height
         
         //convertedRectView = viewUnder.convert(animationView.frame, to: self.view)
         
@@ -37,6 +45,40 @@ class AnimationViewController: UIViewController {
      */
     @IBAction func back(_ sender: Any) {
         dismiss(animated: true, completion: nil)
+    }
+    
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        coordinator.animate(alongsideTransition: { (UIViewControllerTransitionCoordinatorContext) -> Void in
+            
+            let orient = UIApplication.shared.statusBarOrientation
+            
+            switch orient {
+            case .portrait:
+                print("portrait")
+                self.applyPortraitConstraints()
+                break
+            default:
+                print("land")
+                self.applyLandscapeConstraints()
+                break
+            }
+        }, completion: { (UIViewControllerTransitionCoordinatorContext) -> Void in
+            print("rotation finished")
+        })
+        
+        super.viewWillTransition(to: size, with: coordinator)
+        
+    }
+    
+    
+    func applyPortraitConstraints() {
+        self.animViewHeight.constant = vH!/2
+        self.animViewWidth.constant = vW!
+    }
+    
+    func applyLandscapeConstraints() {
+        self.animViewHeight.constant = vW!
+        self.animViewWidth.constant = vH!/2
     }
     
     
